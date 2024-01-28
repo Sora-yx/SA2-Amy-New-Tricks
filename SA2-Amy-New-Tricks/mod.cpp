895c162963a274fc22ef43cb7530d63bc1ca8d29
Sora <sora.x@hotmail.fr>
2024-01-28 18:23:55 +0100

    Deprecated

--- SA2-Amy-New-Tricks/mod.cpp
+++ SA2-Amy-New-Tricks/mod.cpp
@@ -1,31 +1,22 @@
 #include "pch.h"
 
 HelperFunctions HelperFunctionsGlobal;
-const char* error = "[Amy New Tricks]: WARNING: Your version of the Mod Loader is old, the mod won't work properly.\nPlease update your Mod Loader for the best experience.";
+const char* error = "WARNING Amy New Tricks is deprecated! Please download and install 'SA2 New Tricks Mod' for the best experience.\n\nIf you already have it installed, please uninstall Amy New Tricks.";
 
 extern "C" {
 
 	__declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
 	{
-
 		HelperFunctionsGlobal = helperFunctions;
-
-		if (HelperFunctionsGlobal.Version < 8) {
-			PrintDebug(error);
-			MessageBoxA(MainWindowHandle, error, "Amy New Tricks", MB_ICONWARNING);
-		}
-
-		ReadConfig(path); //get mod settings by the user
-		Amy_Init();
-		Init_AmyAnim();
-		Upgrades_Init();
-		init_BassSound();
-		Init_ObjectsHack();
+		PrintDebug(error);
+		MessageBoxA(MainWindowHandle, error, "Amy New Tricks", MB_ICONWARNING);
+		return;
 	}
 
-	__declspec(dllexport) void __cdecl OnFrame() {
-		RunCustomSounds();
-	}	
-	
+	__declspec(dllexport) void __cdecl OnFrame() 
+	{
+
+	}
+
 	__declspec(dllexport) ModInfo SA2ModInfo = { ModLoaderVer };
 }
